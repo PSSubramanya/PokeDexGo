58e366f53e68c3917b8c0b725d0deaf3994bc675
Subramanya <subramanya@robosoftin.com>
2023-04-02 14:35:13 +0530

    Added Event Data Redux files to the app

--- src/actions/count.js
+++
@@ -1,9 +0,0 @@
-// TODO: sample file - Remove Later
-import {COUNTER_CHANGE} from '../constants/types.js';
-
-export function changeCount(count) {
-  return {
-    type: COUNTER_CHANGE,
-    payload: count,
-  };
-}
---
+++ src/reducers/EventDataReducer.js
@@ -0,0 +1,66 @@
+import {
+  EVENT_LINKS,
+  EVENT_SUMMARY,
+  EVENT_START_DATE_TIME,
+  EVENT_IMAGE_URL,
+} from '../constants/types';
+import {combineReducers} from 'redux';
+
+const initialState = {
+  data: {},
+};
+
+const eventlinks = (state = initialState, action) => {
+  switch (action.type) {
+    case EVENT_LINKS:
+      return {
+        ...state,
+        data: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+const eventsummary = (state = initialState, action) => {
+  switch (action.type) {
+    case EVENT_SUMMARY:
+      return {
+        ...state,
+        data: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+const eventtimestamps = (state = initialState, action) => {
+  switch (action.type) {
+    case EVENT_START_DATE_TIME:
+      return {
+        ...state,
+        data: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+const eventimages = (state = initialState, action) => {
+  switch (action.type) {
+    case EVENT_IMAGE_URL:
+      return {
+        ...state,
+        data: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+const eventDataReducer = combineReducers({
+  eventlinks,
+  eventsummary,
+  eventtimestamps,
+  eventimages,
+});
+
+export default eventDataReducer;
