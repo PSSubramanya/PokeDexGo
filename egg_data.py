e67abfadc0b5a33d2482744b390d0b964c3ef415
rahulvhegde <41341512+rahulvhegde@users.noreply.github.com>
2023-04-06 18:02:21 +0530

    Updated webcrapper code with the json file creator

--- egg_data.py
+++ egg_data.py
@@ -1,18 +1,15 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from collections import defaultdict
-from dataclasses import dataclass, field
-from datetime import datetime
-
+from dataclasses import dataclass
 from bs4 import BeautifulSoup
-from dateutil.relativedelta import relativedelta
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.chrome.service import Service
-import pandas
+import pandas as pd
 import requests
+import json
+from bottle import *
+from pivottablejs import pivot_ui
+from ast import literal_eval
+
 
 @dataclass
 class Event:
@@ -35,10 +32,17 @@ class Event:
 def main():
     events = defaultdict()
     driver = webdriver.Chrome(ChromeDriverManager().install())
+
     distance =[]
     imgSrc=[]
     pokemonName=[]
     combatPower=[]
+    data=[]
+    distanceEgg=[]
+    summaries=[]
+    imgSrcEgg=[]
+    pokemonNameEgg=[]
+    combatPowerEgg=[]
 
     url = "https://leekduck.com/eggs/"
 
@@ -71,10 +75,92 @@ def main():
         pokemonName=[]
         combatPower=[]
     
-    df=pandas.DataFrame(events,index=[0]).T
+    df=pd.DataFrame(events,index=[0]).T
     df.columns=['Summary']
+    
     df.to_csv('./egg_data.csv')
-    driver.quit()
+
+    df=pd.read_csv('./egg_data.csv')
+    eggData=pd.DataFrame()
+
+    for i in range(len(df)):
+        distanceEgg.append(df.iloc[i,0])
+        summaries.append(df.iloc[i,1])
+
+    eggData['Distance'] = distanceEgg
+
+    for summary in summaries:
+        summ=summary.split('pokemonName')
+        summ=summ[0].split(':',1)
+        strToBeCleaned=summ[1]
+        strToReplace=','
+        replacementStr=''
+        pos=strToBeCleaned.rfind(strToReplace)
+        if pos >-1:
+            summCleanedSplit=strToBeCleaned[:pos]+replacementStr + strToBeCleaned[pos + len(strToReplace): ]
+        else:
+            summCleanedSplit=strToBeCleaned
+        strToBeCleaned2=summCleanedSplit
+        strToReplace2='\''
+        replacementStr2=''
+        pos2=strToBeCleaned2.rfind(strToReplace2)
+        if pos2 >-1:
+            summCleanedSplit=strToBeCleaned2[:pos2]+replacementStr2 + strToBeCleaned2[pos2 + len(strToReplace2): ]
+        else:
+            summCleanedSplit=strToBeCleaned2
+
+        imgSrcEgg.append(summCleanedSplit)
+
+    for  summary in summaries:
+        summ=summary.split('pokemonName')
+        summ=summ[1].split('combatPower')
+        summ=summ[0].split(':')
+        strToBeCleaned=summ[1]
+        strToReplace=','
+        replacementStr=''
+        pos=strToBeCleaned.rfind(strToReplace)
+        if pos >-1:
+            summCleanedSplit=strToBeCleaned[:pos]+replacementStr + strToBeCleaned[pos + len(strToReplace): ]
+        else:
+            summCleanedSplit=strToBeCleaned
+        strToBeCleaned2=summCleanedSplit
+        strToReplace2='\''
+        replacementStr2=''
+        pos2=strToBeCleaned2.rfind(strToReplace2)
+        if pos2 >-1:
+            summCleanedSplit=strToBeCleaned2[:pos2]+replacementStr2 + strToBeCleaned2[pos2 + len(strToReplace2): ]
+        else:
+            summCleanedSplit=strToBeCleaned2
+        pokemonNameEgg.append(summCleanedSplit)
+
+    for  summary in summaries:
+        summ=summary.split('combatPower')
+        summ=summ[1].split(':',1)
+        summ=summ[1].replace('}','')
+        combatPowerEgg.append(summ)
+
+    eggData['imgSrc'] = imgSrcEgg
+    eggData['pokemonName'] = pokemonNameEgg
+    eggData['combatPower'] = combatPowerEgg
+
+    eggData['json'] = eggData.to_json(orient='records', lines=True).splitlines()
+
+    pivot_ui(eggData)
+
+    for i in eggData['json']:
+        data.append(json.loads(i))
+
+    for i in data:
+        i['imgSrc'] = literal_eval(i['imgSrc'])
+        i['pokemonName'] = literal_eval(i['pokemonName'])
+        i['combatPower'] = literal_eval(i['combatPower'])
+
+    with open('./egg_data.json',"w") as output_file:
+        #output_file.write(pokemon_json.replace('\\','')+'\n')
+        #output_file.write(pokemon_json_cleaned)
+        #output_file.write(json.dumps({"data": pokemonJsonCleaned}, indent=4 ))
+        output_file.write(json.dumps({"data": data}, indent=4 ))
+        driver.quit()
 
 if __name__ == "__main__":
     main()
