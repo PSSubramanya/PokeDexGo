2aa0a03e81fe70db5f86d85326837b04734feea5
Subramanya <subramanya@robosoftin.com>
2023-04-08 16:03:48 +0530

    Removed some unnecessary code

--- src/actions/eventData.js
+++ src/actions/eventData.js
@@ -1,42 +1,8 @@
-import {
-  EVENT_LINKS,
-  EVENT_SUMMARY,
-  EVENT_START_DATE_TIME,
-  EVENT_IMAGE_URL,
-  EVENT_DATA_LOAD,
-} from '../constants/types.js';
+import {EVENT_DATA_LOAD} from '../constants/types.js';
 
 export function eventDataLoad(data) {
   return {
     type: EVENT_DATA_LOAD,
-    payload: data,
-  };
-}
-
-export function eventLinks(data) {
-  return {
-    type: EVENT_LINKS,
-    payload: data,
-  };
-}
-
-export function eventSummary(data) {
-  return {
-    type: EVENT_SUMMARY,
-    payload: data,
-  };
-}
-
-export function eventStartTimeStamp(data) {
-  return {
-    type: EVENT_START_DATE_TIME,
-    payload: data,
-  };
-}
-
-export function eventImageLoad(data) {
-  return {
-    type: EVENT_IMAGE_URL,
     payload: data,
   };
 }
--- src/reducers/EventDataReducer.js
+++ src/reducers/EventDataReducer.js
@@ -1,10 +1,4 @@
-import {
-  EVENT_LINKS,
-  EVENT_SUMMARY,
-  EVENT_START_DATE_TIME,
-  EVENT_IMAGE_URL,
-  EVENT_DATA_LOAD,
-} from '../constants/types';
+import {EVENT_DATA_LOAD} from '../constants/types';
 import {combineReducers} from 'redux';
 
 const initialState = {
@@ -23,57 +17,7 @@ const eventdataload = (state = initialState, action) => {
   }
 };
 
-const eventlinks = (state = initialState, action) => {
-  switch (action.type) {
-    case EVENT_LINKS:
-      return {
-        ...state,
-        data: action.payload,
-      };
-    default:
-      return state;
-  }
-};
-
-const eventsummary = (state = initialState, action) => {
-  switch (action.type) {
-    case EVENT_SUMMARY:
-      return {
-        ...state,
-        data: action.payload,
-      };
-    default:
-      return state;
-  }
-};
-const eventtimestamps = (state = initialState, action) => {
-  switch (action.type) {
-    case EVENT_START_DATE_TIME:
-      return {
-        ...state,
-        data: action.payload,
-      };
-    default:
-      return state;
-  }
-};
-const eventimages = (state = initialState, action) => {
-  switch (action.type) {
-    case EVENT_IMAGE_URL:
-      return {
-        ...state,
-        data: action.payload,
-      };
-    default:
-      return state;
-  }
-};
-
 const eventDataReducer = combineReducers({
-  eventlinks,
-  eventsummary,
-  eventtimestamps,
-  eventimages,
   eventdataload,
 });
 
